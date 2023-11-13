c417caf2d0b00029001bb11af5a85ddfde9555aa
Dylan <dylanosbornebuisness@gmail.com>
2023-11-13 17:10:21 +0200

    Basic "navbar" added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Link, Element, Events, animateScroll as scroll, scrollSpy } from 'react-scroll';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='navbar'>
+        <div className='navbar_left'>
+          <h3>Dylan Osborne</h3>
+        </div>
+
+        <div className='navbar_right'>
+          <Link to="welcome_segment" spy={true} smooth={true} offset={-70} duration={500}>
+            <button>Welcome</button>
+          </Link>
+          <Link to="about_segment" spy={true} smooth={true} offset={-70} duration={500}>
+            <button>About</button>
+          </Link>
+          <Link to="offer_segment" spy={true} smooth={true} offset={-70} duration={500}>
+            <button>Offers</button>
+          </Link>
+          <Link to="contact_segment" spy={true} smooth={true} offset={-70} duration={500}>
+            <button>Contact Info</button>
+          </Link>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Element name="welcome_segment" className='welcome_segment'>
+        {/* Your content for the "Welcome" section */}
+      </Element>
+
+      <Element name="about_segment" className='about_segment'>
+        {/* Your content for the "About" section */}
+      </Element>
+
+      <Element name="offer_segment" className='offer_segment'>
+        {/* Your content for the "Offers" section */}
+      </Element>
+
+      <Element name="contact_segment" className='contact_segment'>
+        {/* Your content for the "Contact Info" section */}
+      </Element>
     </>
   )
 }
